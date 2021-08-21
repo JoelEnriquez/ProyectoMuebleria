7dc01b802fa816c0edd811a4ae0eceb28fc25077
JoelEnriquez <jsem1331@gmail.com>
2021-08-21 12:04:53 -0600

    Add error

--- ProyectMuebleria/src/main/java/Lectura/Error.java
+++ ProyectMuebleria/src/main/java/Lectura/Error.java
@@ -13,4 +13,42 @@ public class Error {
     
     private int numeroLineaError;
     private String tipoError;
+    private String mensajeError;
+
+    public Error(int numeroLineaError, String tipoError, String mensajeError) {
+        this.numeroLineaError = numeroLineaError;
+        this.tipoError = tipoError;
+        this.mensajeError = mensajeError;
+    }
+
+    public Error(int numeroLineaError, String mensajeError) {
+        this.numeroLineaError = numeroLineaError;
+        this.mensajeError = mensajeError;
+    }
+
+    public int getNumeroLineaError() {
+        return numeroLineaError;
+    }
+
+    public void setNumeroLineaError(int numeroLineaError) {
+        this.numeroLineaError = numeroLineaError;
+    }
+
+    public String getTipoError() {
+        return tipoError;
+    }
+
+    public void setTipoError(String tipoError) {
+        this.tipoError = tipoError;
+    }
+
+    public String getMensajeError() {
+        return mensajeError;
+    }
+
+    public void setMensajeError(String mensajeError) {
+        this.mensajeError = mensajeError;
+    }
+    
+    
 }
